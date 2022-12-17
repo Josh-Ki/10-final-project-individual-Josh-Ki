c967465a6be02aeb70c002527a7f62037ae9ac5c
Josh-Ki <j.ki@wustl.edu>
2022-12-16 19:12:38 -0600

    done

--- Anime/script.js
+++ Anime/script.js
@@ -1,76 +1,186 @@
 const API_ENDPOINT = 'https://kitsu.io/api/edge/anime';
 
-// Function to make API request and display the anime image on the screen
+
+
+
+async function getTrendingAnime() {
+  const response = await fetch(`${API_ENDPOINT}?sort=-averageRating&page[limit]=5`);
+  const data = await response.json();
+  return data.data;
+}
+
+async function showTrendingAnime() {
+
+  const trendingAnime = await getTrendingAnime();
+
+
+  const container = document.getElementById('trending-anime');
+
+
+  container.innerHTML = '';
+
+  trendingAnime.forEach(anime => {
+    const animeDiv = document.createElement('div');
+    const title = document.createElement('h2');
+    title.innerHTML = anime.attributes.titles.en_jp;
+    animeDiv.appendChild(title);
+    container.appendChild(animeDiv);
+
+    title.addEventListener('click', () => {
+      console.log("DSLKF")
+      const selectedAnime = document.getElementById('selected-anime');
+
+      const img = document.createElement('img');
+      img.src = anime.attributes.posterImage.small;
+      img.alt = anime.attributes.titles.en_jp;
+
+      const rating = anime.attributes.averageRating / 20;
+
+      const ratingElement = document.createElement('p');
+      ratingElement.innerHTML = `Average Rating: ${rating}/5`;
+      selectedAnime.innerHTML = '';
+   
+      selectedAnime.appendChild(img);
+      selectedAnime.appendChild(ratingElement);
+    });
+  });
+}
+
+
+window.addEventListener('load', showTrendingAnime);
+
 async function searchAnime() {
-  // Get the anime title entered by the user
   const animeTitle = document.getElementById('search-input').value;
 
-  // Make the API request to search for the anime
   const response = await fetch(`${API_ENDPOINT}?filter[text]=${animeTitle}`);
   const data = await response.json();
 
-  // Clear any existing search results
   const searchResults = document.getElementById('search-results');
   searchResults.innerHTML = '';
 
-  // Check if the search returned any results
   if (data.data.length > 0) {
-    // Create an unordered list element for the search results
     const list = document.createElement('ul');
 
-    // Loop through the results
-    data.data.forEach(anime => {
-      // Create a list item element
-      const listItem = document.createElement('li');
-
-      // Create a `h2` element to display the anime title
-      const title = document.createElement('h2');
-      title.innerHTML = anime.attributes.titles.en_jp;
-
-      // Add an event listener to the title to handle selection
-      title.addEventListener('click', () => {
-        // Clear the selected anime
-        searchResults.innerHTML = '';
-        const selectedAnime = document.getElementById('selected-anime');
-        selectedAnime.innerHTML = '';
-
-        // Create an image element to display the anime image
-        const img = document.createElement('img');
-        img.src = anime.attributes.posterImage.small;
-        img.alt = anime.attributes.titles.en_jp;
-
-        // Convert the rating to be out of 5 stars
-        const rating = anime.attributes.averageRating / 2;
-
-        // Create a `p` element to display the anime's average rating
-        const ratingElement = document.createElement('p');
-        ratingElement.innerHTML = `Average Rating: ${rating/20}/5`;
-
-        // Add the image, title, and rating to the selected anime element
-        selectedAnime.appendChild(title);
-        selectedAnime.appendChild(img);
-        selectedAnime.appendChild(ratingElement);
+    const selectedAnime1 = document.getElementById('selected-anime-1');
+    const selectedAnime2 = document.getElementById('selected-anime-2');
+        data.data.forEach(anime => {
+          const listItem = document.createElement('li');
+          const title = document.createElement('h2');
+          title.innerHTML = anime.attributes.titles.en_jp;
+    
+          title.addEventListener('click', () => {
+            searchResults.innerHTML = '';
+            selectedAnime1.innerHTML = '';
+            selectedAnime2.innerHTML = '';
+    
+            const img = document.createElement('img');
+            img.src = anime.attributes.posterImage.small;
+            img.alt = anime.attributes.titles.en_jp;
+    
+            const rating = anime.attributes.averageRating / 20;
+    
+            const ratingElement = document.createElement('p');
+            ratingElement.innerHTML = `Average Rating: ${rating}/5`;
+        if (selectedAnime1.innerHTML === '') {
+          selectedAnime1.appendChild(title);
+          selectedAnime1.appendChild(img);
+          selectedAnime1.appendChild(ratingElement);
+        } else if (selectedAnime2.innerHTML === '') {
+          selectedAnime2.appendChild(title);
+          selectedAnime2.appendChild(img);
+          selectedAnime2.appendChild(ratingElement);
+        }
       });
 
-      // Add the title to the list item
       listItem.appendChild(title);
 
-      // Add the list item to the list
       list.appendChild(listItem);
     });
 
-    // Add the list to the search results element
     searchResults.appendChild(list);
   } else {
-    // If the search returned no results, display an error message
     const p = document.createElement('p');
     p.innerHTML = 'No anime found.';
     searchResults.appendChild(p);
   }
 }
 
+    
 
-
-// Listen for clicks on the search button
+//https://medium.com/@nitinpatel_20236/challenge-of-building-a-calendar-with-pure-javascript-a86f1303267d for reference
 const searchButton = document.getElementById('search-button');
 searchButton.addEventListener('click', searchAnime);
+
+async function showCalendar() {
+
+  const response = await fetch(`${API_ENDPOINT}?startDate&page[limit]=20&filter[status]=upcoming`);
+  const data = await response.json();
+ 
+
+
+  const calendarBody = document.getElementById('calendar-body');
+
+
+  calendarBody.innerHTML = '';
+
+
+  const currentDate = new Date();
+
+  let calendarDate = new Date(currentDate.getFullYear(), currentDate.getMonth(), 1);
+
+
+  const firstDayOfMonth = calendarDate.getDay();
+
+
+  const daysInMonth = new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 0).getDate();
+
+
+  let row = document.createElement('tr');
+
+
+  for (let i = 0; i < firstDayOfMonth; i++) {
+    const cell = document.createElement('td');
+    row.appendChild(cell);
+  }
+
+  for (let i = 1; i < daysInMonth; i++) {
+
+    const cell = document.createElement('td');
+    cell.innerHTML = i;
+
+
+    data.data.forEach(anime => {
+
+      const startDate = new Date(anime.attributes.startDate);
+
+      if (startDate.getFullYear() === calendarDate.getFullYear() && startDate.getDate() === calendarDate.getDate()&& startDate.getMonth() === calendarDate.getMonth() ) {
+     
+                const title = document.createElement('p');
+                title.innerHTML = anime.attributes.titles.en_jp;
+        
+               
+                cell.appendChild(title);
+              }
+            });
+        
+          
+            row.appendChild(cell);
+        
+          
+            if (calendarDate.getDay() === 6) {
+              calendarBody.appendChild(row);
+              row = document.createElement('tr');
+            }
+        
+          
+            calendarDate.setDate(calendarDate.getDate() + 1);
+          }
+        
+     
+          calendarBody.appendChild(row);
+        }
+        
+
+
+
+showCalendar();
